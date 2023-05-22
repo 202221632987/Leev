2abc764f90d2b4d2915c08198072ddda64f96158
202221632987 <134258177+202221632987@users.noreply.github.com>
2023-05-22 16:28:53 +0800

    Update make_code_FFmp.py

--- pre_code/make_code_FFmp.py
+++ pre_code/make_code_FFmp.py
@@ -7,22 +7,21 @@ with open('../function.json', 'r') as f:
     data_total = json.load(f)
 i = 0
 print("\n"+str(len(data_total)))
-if not os.path.exists("raw_code_FFmp"):
-    os.mkdir("raw_code_FFmp")
+if not os.path.exists("../data/raw_code_FFmp"):
+    os.mkdir("../data/raw_code_FFmp")
 for data in tqdm(data_total):
     filename = str(data["commit_id"])+"_"+str(data["target"])+".c"
-    # 文件有重名现象
-    if os.path.exists("raw_code_FFmp"+"/"+filename):
-        with open("raw_code_FFmp"+"/"+filename, 'r') as f:
+    if os.path.exists("../data/raw_code_FFmp"+"/"+filename):
+        with open("../data/raw_code_FFmp"+"/"+filename, 'r') as f:
             func = f.read()
             if func == data["func"]:
                 print(filename)
                 continue
             else:
-                with open("raw_code_FFmp" + "/" + str(i)+"_"+str(data["target"])+".c", 'w') as f:
+                with open("../data/raw_code_FFmp" + "/" + str(i)+"_"+str(data["target"])+".c", 'w') as f:
                     f.write(data["func"])
             i = i + 1
-    with open("raw_code_FFmp"+"/"+filename, 'w') as f:
+    with open("../data/raw_code_FFmp"+"/"+filename, 'w') as f:
         f.write(data["func"])
 print(i)
 
